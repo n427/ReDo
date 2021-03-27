469c719a1e2de16bc025fed476415bb43cf4b30b
David Zhang <davidhqr@gmail.com>
2021-03-26 17:39:06 -0700

    Add firebase code

--- app/src/main/java/com/example/redo/ListingsActivity.java
+++ app/src/main/java/com/example/redo/ListingsActivity.java
@@ -1,15 +1,22 @@
 package com.example.redo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
-public class ListingsActivity extends AppCompatActivity implements View.OnClickListener {
+public class ListingsActivity extends AppCompatActivity implements View.OnClickListener, BottomNavigationView.OnNavigationItemReselectedListener {
 
     private ArrayAdapter<String> listAdapter;
 
@@ -17,6 +24,9 @@ public class ListingsActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listings);
+
+        BottomNavigationView bottomNavigation =findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemReselectedListener(this);
 
         ListView listView = findViewById(R.id.listingsList);
         ArrayList<String> listData = new ArrayList<>();
@@ -31,7 +41,20 @@ public class ListingsActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                break;
+        }
+    }
 
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent mainIntent = new Intent(ListingsActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                break;
         }
     }
 }
